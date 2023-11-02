5f05d6f01a9acdccd15a1d5e85959faa723d21b1
revectores <revectores@gmail.com>
2023-11-02 20:03:22 +0800

    init linkbuffer

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/revectores/linkbuffer
+
+go 1.18
+
+require github.com/bytedance/gopkg v0.0.0-20230728082804-614d0af6619b
---
+++ test_common.go
@@ -0,0 +1,45 @@
+package linkbuffer
+
+import "testing"
+
+func MustNil(t *testing.T, val interface{}) {
+	t.Helper()
+	Assert(t, val == nil, val)
+	if val != nil {
+		t.Fatal("assertion nil failed, val=", val)
+	}
+}
+
+func MustTrue(t *testing.T, cond bool) {
+	t.Helper()
+	if !cond {
+		t.Fatal("assertion true failed.")
+	}
+}
+
+func Equal(t *testing.T, got, expect interface{}) {
+	t.Helper()
+	if got != expect {
+		t.Fatalf("assertion equal failed, got=[%v], expect=[%v]", got, expect)
+	}
+}
+
+func Assert(t *testing.T, cond bool, val ...interface{}) {
+	t.Helper()
+	if !cond {
+		if len(val) > 0 {
+			val = append([]interface{}{"assertion failed:"}, val...)
+			t.Fatal(val...)
+		} else {
+			t.Fatal("assertion failed")
+		}
+	}
+}
+
+func TestEqual(t *testing.T) {
+	var err error
+	MustNil(t, err)
+	MustTrue(t, err == nil)
+	Equal(t, err, nil)
+	Assert(t, err == nil, err)
+}
